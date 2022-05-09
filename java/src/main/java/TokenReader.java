ab8e20f845c434a357438114fa69df0415c64214
Alejandro Moya <alejandro144ago@gmail.com>
2022-05-09 10:43:59 -0500

    Creamos la clase TokenReader y agregamos los archivos .secret

--- java/src/main/java/TokenReader.java
+++ java/src/main/java/TokenReader.java
@@ -1,2 +1,73 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TokenReader {
+
+    public static String readUserName (){
+        String salida = "";
+        try {
+            BufferedReader buffer = new BufferedReader(new FileReader("userNameDocHub.secret"));
+            salida = buffer.readLine();
+            buffer.close();
+        }catch (FileNotFoundException e){
+            System.out.println("Error: Fichero no encontrado");
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return salida;
+    }
+
+    public static String readToken (){
+        String salida = "";
+        try {
+            BufferedReader buffer = new BufferedReader(new FileReader("tokenDocHub.secret"));
+            salida = buffer.readLine();
+            buffer.close();
+
+        }catch (FileNotFoundException e){
+            System.out.println("Error: Fichero no encontrado");
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return salida;
+    }
+
+    public static String dataBase (){
+        String salida = "";
+        try {
+            BufferedReader buffer = new BufferedReader(new FileReader("tokenDataBase.secret"));
+            salida = buffer.readLine();
+            buffer.close();
+
+        }catch (FileNotFoundException e){
+            System.out.println("Error: Fichero no encontrado");
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return salida;
+    }
+
+    public static String administrator(){
+        String salida = "";
+        try {
+            BufferedReader buffer = new BufferedReader(new FileReader("administrator.secret"));
+            salida = buffer.readLine();
+            buffer.close();
+
+        }catch (FileNotFoundException e){
+            System.out.println("Error: Fichero no encontrado");
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return salida;
+    }
+
+
+
 }
