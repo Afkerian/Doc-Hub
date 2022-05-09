afb65e517f7478aad00500663cf7198108d89a93
Alejandro Moya <alejandro144ago@gmail.com>
2022-05-09 10:48:19 -0500

    Creamos la clase DocHub para el bot e instanciamos en la clase principal el bot

---
+++ java/src/main/java/DocHub.java
@@ -0,0 +1,21 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class DocHub extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return TokenReader.readUserName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return TokenReader.readToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+
+    }
+}
--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,6 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+public class Main {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(new DocHub());
+        System.out.println("Hello, world!");
     }
 }
